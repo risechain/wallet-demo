f1699e2ce42b9a115cdce4e318a7ed4fe71cbf65
Jen <jen@robolabs.biz>
2025-12-22 12:53:40 +0800

    fix approved amount and error handling

--- packages/nextjs/wallet-playground/src/hooks/useTransaction.ts
+++ packages/nextjs/wallet-playground/src/hooks/useTransaction.ts
@@ -99,15 +99,24 @@ export function useTransaction() {
         version: "1",
       } as any);
 
-      const id = result.id
+      const hash = result.id
         ? await getTransactionHash(result.id as Address, provider)
         : "";
 
-      return {
-        success: true,
-        error: null,
-        data: { ...result, id, usedSessionKey: false },
-      };
+      // TODO: percept status 500 only?
+      const id = hash.receipts[0].transactionHash;
+      if (hash.status === 500) {
+        return {
+          success: false,
+          data: { ...result, id, usedSessionKey: true },
+        };
+      } else {
+        return {
+          success: true,
+          error: null,
+          data: { ...result, id, usedSessionKey: true },
+        };
+      }
     } catch (error) {
       console.log("execute-with-passkey-error: ", error);
 
@@ -166,7 +175,7 @@ export function useTransaction() {
         ],
       });
 
-      const id =
+      const hash =
         result.length === 0
           ? ""
           : await getTransactionHash(result[0].id, provider);
@@ -184,11 +193,20 @@ export function useTransaction() {
       console.log("session-digest:: ", digest);
       console.log("session-key:: ", key);
 
-      return {
-        success: true,
-        error: null,
-        data: { ...resp, id, usedSessionKey: true },
-      };
+      // TODO: percept status 500 only?
+      const id = hash.receipts[0].transactionHash;
+      if (hash.status === 500) {
+        return {
+          success: false,
+          data: { ...resp, id, usedSessionKey: true },
+        };
+      } else {
+        return {
+          success: true,
+          error: null,
+          data: { ...resp, id, usedSessionKey: true },
+        };
+      }
     } catch (error) {
       console.log("execute-with-sessionkey-error:: ", error);
       return {
@@ -206,11 +224,13 @@ export function useTransaction() {
       params: [id],
     });
 
-    if (hash.receipts.length !== 0) {
-      return hash.receipts[0].transactionHash;
-    }
+    console.log("hash:: ", hash);
 
-    return "";
+    // if (hash.receipts.length !== 0) {
+    //   return hash.receipts[0].transactionHash;
+    // }
+
+    return hash;
   }
 
   async function getEthAccounts() {
